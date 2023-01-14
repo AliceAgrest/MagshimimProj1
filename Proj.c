3154978f88cdd8a0ad83ca32dce9bae76db234e1
Alice Agrest <114485470+AliceAgrest@users.noreply.github.com>
2023-01-14 21:05:01 +0200

    Update Proj.c

--- Proj.c
+++ Proj.c
@@ -6,61 +6,71 @@
 enum difficultLevel {EASY = 1, MEDIUM, HARD, ULTRA_HARD};
 
 void rules(void);
+void theLion (void);
 int creditCodeRnd(void);
 enum difficultLevel selectOptions(void);
 int difficultyTries(enum difficultLevel difficulty);
+int inRange (int guessCode);
 bool doesDigitExist (int credit,int creditCode);
 int creditCodeRnd (void);
-int duplictesNumberCount (int guessCode);
+bool duplictesNumberCount (int guessCode);
+int anotherGuess (int guessCode, bool duplication);
 void correctNhalfGuess (int creditCode, int guessCode);
-bool toContinue (void);
 
 #define MAX_NUM 6
 #define MIN_NUM 1
 #define DIGIT_COUNT 4
 
 int main(void) {
-	/*srand(time(NULL));
+	srand(time(NULL));
 	bool dontStop = true;
-	while (dontStop){
+	while (dontStop == true){
 		rules();
+		theLion();
 		int creditCode = creditCodeRnd();
-		int tries = difficultyTries(selectOptions());
+		int option = selectOptions();
+		int tries = difficultyTries(option);
 		int guessCode;
-		int duplicateNumCount;
+		bool duplication = false;
+		char toContinue;
+		bool toContinue1 = true;
 
-		for (int i = 1; i <= tries; i++){
-			printf("You have %d tries\n", tries);
-			puts("Enter a guess for a code: ");
-			scanf("%d", &guessCode);
-
-			duplicateNumCount = duplictesNumberCount(guessCode);
-			tries -= duplicateNumCount;
+		for (; tries > 0; tries--){
+			if (option != ULTRA_HARD)
+				printf("You have %d tries\n", tries);
+			puts("Enter a guess for the code: ");
+			scanf(" %d", &guessCode);
+			guessCode = inRange(guessCode);
+			duplication = duplictesNumberCount(guessCode);
+			guessCode = anotherGuess(inRange(guessCode), duplication);
 
 			if (creditCode == guessCode){
 				printf("You win!!! The credit code was %d\nCongratulations!\n", creditCode);
 				break;
 			}
 			else{
-				correctNhalfGuess(creditCode, guessCode);
+				if (option != ULTRA_HARD)
+					correctNhalfGuess(creditCode, guessCode);
 				continue;
 			}
 		}
-		if (creditCode == guessCode)
+		if (creditCode == guessCode && tries == 0)
 			printf("You win!!!\nThe credit code was %d\nCongratulations!\n", creditCode);
 		else if (creditCode != guessCode)
 			printf("You lost :(\nThe credit Code was %d\nMaybe next time\n", creditCode);
 		
-		dontStop = toContinue();
-	}*/
-	int creditCode = 1234;
-	int guess = 1243;
-	correctNhalfGuess(creditCode, guess);
+		puts("Do you want go to another round?\ny - yes\nn - no");
+		scanf(" %c", &toContinue);
+		if (toContinue == 'n')
+			dontStop = false;
+		else
+			dontStop = true;
+	}
 	return 0;
 }
 
 //explains the rules of the game
-void rules(void) {
+void rules (void) {
 	puts("Welcome to the \"Guess The Antiochus's Code\" game");
 	puts("You have a mission, you need to discover the credit's card code!");
 	puts("Here some rules:");
@@ -68,6 +78,24 @@ void rules(void) {
 	puts("2 - if you put two same numbers, you'll lose a guess");
 	puts("3 - correct guess means that you put the right number in right place");
 	puts("4 - half guess means that you put the right number in wrong place");
+}
+
+//prints lion
+void theLion (void){
+	puts("	      ,  ,, ,");
+	puts(" 	   , ,; ; ;;  ; ;  ;");
+	puts("         , ; ';  ;  ;; .-''\\ ; ;");
+	puts("	, ;  ;`  ; ,; . / /8b \\ ; ;");
+	puts("	`; ; .;'         ;,\\8 |  ;  ;");
+	puts("	` ;/   / `_      ; ;;    ;  ; ;");
+    puts("	   |/.'  /9)    ;  ; `    ;  ; ;");
+	puts("	  ,/'          ; ; ;  ;   ; ; ; ;");
+	puts("	 /_            ;    ;  `    ;  ;");
+	puts("	`?8P\"  .      ;  ; ; ; ;     ;  ;;");
+	puts("	| ;  .:: `     ;; ; ;   `  ;  ;");
+	puts("	`' `--._      ;;  ;;  ; ;   ;   ;");
+	puts("	`-..__..--''   ; ;    ;;   ; ;   ;");
+    puts("			;    ; ; ;   ;     ;");
 }
 
 //you enter which level you want then it returns into the enum difficulty level
@@ -92,7 +120,7 @@ enum difficultLevel selectOptions (void){
 			case 4:
 				return ULTRA_HARD;
 			default:
-				puts ("Invalid option!!!!!!!!!!!!!!\nTry again blyat");
+				puts ("Invalid option!!!!!!!!!!!!!!\nTry again ");
 		}
 	}
 }
@@ -136,19 +164,55 @@ bool doesDigitExist (int credit,int creditCode){
 	return false;
 }
 
-//checks that there is no duplicate numbers in input
-int duplictesNumberCount (int guessCode){
-	int guessCopy = guessCode;
-	int duplicateCount = 0;
-
-	for (int i = 1; i <= DIGIT_COUNT; guessCode /= 10, i++){
+//checks that the input is in range
+int inRange (int guessCode){
+	bool toContinue;
+	int inRange = 0; 
+	do {
 		int guessCopy = guessCode;
-		for (int x = 1; x <= DIGIT_COUNT; guessCopy /= 10, x++){
-			if(guessCode % 10 == guessCopy % 10)
-				duplicateCount ++;
+		for (int i = 1; i <= DIGIT_COUNT; guessCopy /= 10, i++){
+			if (guessCopy % 10 >= 1 && guessCopy % 10 <= 6)
+				inRange ++;
 		}
+		if (inRange != DIGIT_COUNT){
+			puts("Invaild input!!!!!!!!!!\nEnter another guess: ");
+			scanf(" %d", &guessCode);
+			continue;
+		}
+		else 
+			break;
+	} while (toContinue);
+	return guessCode;
+}
+
+//checks that there is no duplicate numbers in input
+bool duplictesNumberCount (int guessCode){
+	bool duplication = false;
+	int duplicateCount = 0;
+	int t = guessCode / 1000;//t - thousands
+	int h = (guessCode / 100) % 10;//h - hundreds
+	int d = (guessCode / 10) % 10;//d - dozens
+	int u = guessCode % 10;//u - units
+
+	if (t == h || t == d || t == u)
+		duplicateCount++;
+	else if (h == d || h == u)
+		duplicateCount++;
+	else if (d == u)
+		duplicateCount++;
+
+	duplication = (duplicateCount > 0) ? true : false;
+	return duplication;
+}
+
+//if the input has duplicates the func asks for another input also check if the other iput is in range
+int anotherGuess (int guessCode, bool duplication){
+	while(duplication == true){
+		puts ("Invaild input!!!!!!!!!!\nEnter another guess code: ");
+		scanf(" %d", &guessCode);
+		duplication = duplictesNumberCount(guessCode);
 	}
-	return duplicateCount;
+	return guessCode;
 }
 
 //checks the user guess code with genereted credit code
@@ -162,7 +226,7 @@ void correctNhalfGuess (int creditCode, int guessCode){
 	
 	for (int i = 1; i <= DIGIT_COUNT; creditCopy /= 10, i++){
 		guessCopy = guessCode;
-		for (int x = 1; i <= DIGIT_COUNT; guessCopy /= 10, x++){
+		for (int x = 1; x <= DIGIT_COUNT; guessCopy /= 10, x++){
 			if (i == x && creditCopy % 10 == guessCopy % 10){
 				printf("Correct guess number %d\n", guessCopy % 10);
 				correctNumCount ++;
@@ -175,16 +239,4 @@ void correctNhalfGuess (int creditCode, int guessCode){
 	}
 	printf ("The count of correct numbers count is %d\n", correctNumCount);
 	printf ("The count of half-correct numbers count is %d\n", halfNumCount);
-}
-
-//the func asks if the user want to play another round or not
-bool toContinue (void){
-	char choice;
-	puts("Do you want another round?\ny - yes\nn - no");
-	scanf("%c", &choice);
-
-	if (choice == 'y')
-		return true;
-	else
-		return false;
 }
